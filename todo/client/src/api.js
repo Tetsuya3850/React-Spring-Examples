109f1d6fea0c480899b4b185bb37504f3d2dc17a
Tetsuya Hasegawa <tetsuya.chicago@gmail.com>
2019-04-09 20:33:15 +0900

    deployed

--- todo/client/src/api.js
+++ todo/client/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const SERVER_URL = "http://localhost:8080";
+const SERVER_URL = "https://todo-server-spring-3850.herokuapp.com";
 axios.defaults.baseURL = SERVER_URL;
 
 export const fetchTodos = () => axios.get(`/todos`);
