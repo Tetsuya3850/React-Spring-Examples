c3fb5282afe641da2fd34a98c634102672f3a6eb
Tetsuya Hasegawa <tetsuya.chicago@gmail.com>
2019-04-13 09:16:41 +0900

    wrote test

---
+++ todo/server/src/test/java/com/example/todoserver/FormTodo.java
@@ -0,0 +1,14 @@
+package com.example.todoserver;
+
+public class FormTodo {
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+}
--- todo/server/src/test/java/com/example/todoserver/TodoServerApplicationTests.java
+++ todo/server/src/test/java/com/example/todoserver/TodoServerApplicationTests.java
@@ -1,39 +1,65 @@
 package com.example.todoserver;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class TodoServerApplicationTests {
 
+    // https://stackoverflow.com/questions/17143116/integration-testing-posting-an-entire-object-to-spring-mvc-controller
+    // https://github.com/spring-projects/spring-boot/issues/7321
+    // https://github.com/spring-projects/spring-framework/blob/master/spring-test/src/test/java/org/springframework/test/web/servlet/samples/standalone/resultmatchers/JsonPathAssertionTests.java
+
     @Autowired
     private MockMvc mvc;
 
-    @Test
-    public void postFailTest() throws Exception {
-        this.mvc.perform(post("/todos").param("text", "")).andExpect(status().isOk());
+    public static String asJsonString(final Object obj) {
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            final String jsonContent = mapper.writeValueAsString(obj);
+            return jsonContent;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Test
-    public void postSuccessTest() throws Exception {
-        this.mvc.perform(post("/todos").param("text", "swim")).andExpect(status().isOk());
-    }
+    public void test() throws Exception {
+        FormTodo newTodo = new FormTodo();
 
-    @Test
-    public void getTest() throws Exception {
-        this.mvc.perform(get("/todos")).andExpect(status().isOk());
+        this.mvc.perform(post("/todos")
+                .content(asJsonString(newTodo))
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().is4xxClientError());
+
+        newTodo.setText("swim");
+
+        this.mvc.perform(post("/todos")
+                .content(asJsonString(newTodo))
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk()).andExpect(jsonPath("$.text").value("swim"));
+
+        this.mvc.perform(get("/todos")).andExpect(status().isOk()).andExpect(jsonPath("$[0].text").value("swim"));
+
+        this.mvc.perform(delete("/todos/{id}", 1)).andExpect(status().isOk());
+
+        this.mvc.perform(get("/todos")).andExpect(status().isOk()).andExpect(jsonPath("$[1]").doesNotExist());
+
     }
 
 }
